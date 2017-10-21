2c76fae6f8b25995fa37057458b351bc8650444c
LhuiOSU <lhlk2233@gmail.com>
2017-10-21 10:29:13 -0600

    Add files via upload

---
+++ ProfClass.py
@@ -0,0 +1,14 @@
+class Prof:
+    department = 0;
+    score = 0;
+    name = 0;
+    numberRate = 0;
+
+    def __init__(self, d, s, n, nR):
+        self.department = d
+        self.score = s
+        self.name = n
+        self.numberRate = nR
+
+
+
