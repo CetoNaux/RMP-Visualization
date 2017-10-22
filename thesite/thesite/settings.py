8052333f4b73ed9e2491f6b10190c57e0408a37f
Xuan, Shichun <codexuan@gmail.com>
2017-10-21 21:04:58 -0400

    Website updated

--- thesite/thesite/settings.py
+++ thesite/thesite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'thesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'mysql.connector.django',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'test',
+        'PASSWORD': 'test123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- thesite/thesite/view.py
+++ thesite/thesite/view.py
@@ -4,8 +4,10 @@
 from django.shortcuts import render
  
 def home(request):
-
     return render(request, 'home.html')
 
 def start(request):
 	return render(request, 'start.html')
+
+def data(request):
+	return render(request, 'data.html')
